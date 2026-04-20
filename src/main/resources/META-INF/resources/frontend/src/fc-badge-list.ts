7a1b959f5ed1a3422740e104d1f1f66a11c8fee3
Paola De Bartolo <paola.debartolo@gmail.com>
2026-04-20 18:13:28 -0300

    fix: use explicit marginInlineStart/End for overflow badge
    Close #41

--- src/main/resources/META-INF/resources/frontend/src/fc-badge-list.ts
+++ src/main/resources/META-INF/resources/frontend/src/fc-badge-list.ts
@@ -165,7 +165,7 @@ export class BadgeList extends ResizeMixin(ThemableMixin(LitElement)) {
     // Reset overflow badge for overflow calculations and get width
     overflow.removeAttribute("hidden");
     let overflowStyle = getComputedStyle(overflow);
-    let overflowWidth = overflow.offsetWidth + parseInt(overflowStyle.marginInline);
+    let overflowWidth = overflow.offsetWidth + parseInt(overflowStyle.marginInlineStart) + parseInt(overflowStyle.marginInlineEnd);
 
     // Reset badges for calculations
     badges.forEach(badge => {
